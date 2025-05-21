96305bfe21eedb335a675759ae967320fbfe9dc2
Novikov Gleb <pracagep@gmail.com>
2025-05-21 12:13:02 +0300

    Pacticum book was added

--- 2SEM/KPO/KPO 7/calls.cpp
+++ 2SEM/KPO/KPO 7/calls.cpp
@@ -1,9 +1,9 @@
 #include "calls.h"
 
-int _cdecl(int first, int second, int third){
+int _cdecl(int first, int second, int third) {
     return first + second + third;
 }
-int _stdcall(int *first, int second, int third){
+int _stdcall(int* first, int second, int third) {
     int num1 = *first;
     return num1 * second * third;
 }
